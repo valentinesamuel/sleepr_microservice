d8381ff3f6d890d23f60c2dda1a116fec4460df5
valentinesamuel2580 <valentinesamuel2580@gmail.com>
2023-07-21 11:32:11 +0100

    FEAT: generated resource for the reservation microservice

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReservationsController } from './reservations.controller';
 import { ReservationsService } from './reservations.service';
+import { DatabaseModule } from '@app/common';
+import { ReservationsModule } from './reservations/reservations.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, ReservationsModule],
   controllers: [ReservationsController],
   providers: [ReservationsService],
 })
