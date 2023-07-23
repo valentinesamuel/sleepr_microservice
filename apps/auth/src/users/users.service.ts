e1d193881a4fd72a3e4432db70d2c2913eacdac5
valentinesamuel2580 <valentinesamuel2580@gmail.com>
2023-07-23 19:25:18 +0100

    FEAT: Implemented jwt strategy and preventing duplicate email

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,6 +1,10 @@
 import { UsersRepository } from './users.repository';
 import * as bcrypt from 'bcryptjs';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { GetUserDto } from './dto/get-user.dto';
 
@@ -8,10 +12,23 @@ import { GetUserDto } from './dto/get-user.dto';
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
   async create(createUserDTO: CreateUserDto) {
+    await this.validateCreateUserDto(createUserDTO);
     return this.usersRepository.create({
       ...createUserDTO,
       password: await bcrypt.hash(createUserDTO.password, 10),
     });
+  }
+
+  private async validateCreateUserDto(createUserDto: CreateUserDto) {
+    try {
+      await this.usersRepository.findOne({ email: createUserDto.email });
+      console.log('EXISTS');
+    } catch (err) {
+      console.log('NOT FOUND');
+      return;
+    }
+    console.log('NOT PROCESSABLE');
+    throw new UnprocessableEntityException('Email Already exists');
   }
 
   async validateUser(email: string, password: string) {
