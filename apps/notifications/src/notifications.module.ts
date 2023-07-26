f59714230a05e59466ed943086d9dbdb660af346
valentinesamuel2580 <valentinesamuel2580@gmail.com>
2023-07-26 19:33:54 +0100

    Setup Module and added .env variables

--- apps/notifications/src/notifications.module.ts
+++ apps/notifications/src/notifications.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from '@app/common';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+      }),
+    }),
+    LoggerModule,
+  ],
   controllers: [NotificationsController],
   providers: [NotificationsService],
 })
